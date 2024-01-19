01f11a35684c9e915353d73e7251cee7deefc13f
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-01-19 13:29:43 -0600

    Add deployment workflow, Dockerfile, docker-compose.yml, redeploy script, and NGINX configuration

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20.10.0-alpine
+
+WORKDIR /kofy-dashboard
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+RUN npx prisma generate
+
+CMD ["npm", "run", "build", "&&", "npm", "start"]
+
+EXPOSE 3005
---
+++ redeploy-site.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# cd into projects folder
+cd ~/Kofy/kofy-dashboard
+
+# git stach
+git stash
+
+# git fetch
+git pull
+
+sudo chmod 700 redeploy-site.sh
+
+# Spin docker containers down to prevent errors
+docker compose -f docker-compose.yml down --remove-orphans
+
+# Compose docker containers back up
+docker compose -f docker-compose.yml up -d --build
+
+echo "Dashboard Re-deployed!"
