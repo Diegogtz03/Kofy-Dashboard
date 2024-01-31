6813ac0108bb6bdf328c4945cb250ffc8bf2d7a5
RoJosGaRis <roberto.jgr03@gmail.com>
2024-01-31 16:30:07 -0600

    Add HowTo.js component and update styles

--- src/components/HowTo/HowTo.js
+++ src/components/HowTo/HowTo.js
@@ -1,5 +1,9 @@
 import styles from "./HowTo.module.css";
 import Image from "next/image";
+import localFont from "next/font/local";
+const zenTokyo = localFont({
+  src: "../../pages/fonts/ZenTokyoZoo-Regular.ttf",
+});
 
 function HowTo({ setIsOpen }) {
   return (
@@ -8,66 +12,28 @@ function HowTo({ setIsOpen }) {
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
-            <h5 className={styles.heading}>¿Cómo funciona KOFY?</h5>
+            <h5 className={`${styles.heading} ${zenTokyo.className}`}>
+              CÓMO FUNCIONA KOFY
+            </h5>
           </div>
           <div className={styles.modalContent}>
             <ol>
               <li>
                 <p>Ingresa el ID de sesión que le proporcionó el paciente.</p>
-                <Image
-                  src="/media/gif1.gif"
-                  className={styles.gif}
-                  width={300}
-                  height={100}
-                  alt="icono de flecha"
-                />
               </li>
               <li>
                 Una vez dentro de la sesión, verá el resumen de la sesión y
                 podrá realizar las siguientes tres operaciones:
                 <ul>
-                  <li>
-                    Agregar información al resumen
-                    <Image
-                      src="/media/gif1.gif"
-                      className={styles.gif}
-                      width={300}
-                      height={100}
-                      alt="icono de flecha"
-                    />
-                  </li>
-                  <li>
-                    Eliminar información del resumen
-                    <Image
-                      src="/media/gif1.gif"
-                      className={styles.gif}
-                      width={300}
-                      height={100}
-                      alt="icono de flecha"
-                    />
-                  </li>
-                  <li>
-                    Modificar información del resumen
-                    <Image
-                      src="/media/gif1.gif"
-                      className={styles.gif}
-                      width={300}
-                      height={100}
-                      alt="icono de flecha"
-                    />
-                  </li>
+                  <li>Agregar información al resumen</li>
+                  <li>Eliminar información del resumen</li>
+                  <li>Modificar información del resumen</li>
                 </ul>
               </li>
               <li>
                 Al terminar la revisión del contenido, podrá validar la sesión y
                 enviar el resumen al paciente.
-                <Image
-                  src="/media/gif1.gif"
-                  className={styles.gif}
-                  width={300}
-                  height={100}
-                  alt="icono de flecha"
-                />
+                <br />
               </li>
             </ol>
           </div>
